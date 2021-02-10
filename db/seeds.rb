1508f01779ea6903a7eee7a158f6a172493a76a4
ROBYN PERRIN <robynperrin76@gmail.com>
2021-02-10 14:48:45 +0100

    Done yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+Review.destroy_all
+
+10.times do
+  my_array = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: my_array.sample
+  )
+  review = Review.new(
+    rating: rand(0..5),
+    content: Faker::Lorem.paragraph
+  )
+  review.restaurant = restaurant
+  restaurant.save
+  review.save
+  puts restaurant.name
+  puts review.rating
+  puts review.content
+end
+puts 'Seed Done'
+
